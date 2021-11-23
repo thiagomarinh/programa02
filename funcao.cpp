1aa5e7962fcfcd6e48ab5e067aee3c868b04b8b4
thiagomarinh <thiagoo_100@hotmail.com>
2021-11-23 16:50:42 -0300

    inclusão range-based-for

--- funcao.cpp
+++ funcao.cpp
@@ -1,20 +1,31 @@
 #include <iostream> 
 #include <string>  
+#include <cstdlib>
 using namespace std;
 
 void incrementaNumero(int &);
+string forRange(string);
 
 int main(){
-    int numero = 1;
+    int numero = rand();
     incrementaNumero(numero);
     
+    
+}
+
+string forRange(string){    
+    string str = "Mundo";
+
+    for(char c:str){
+        std::cout << c << endl;
+    }
 }
 
 
 void incrementaNumero(int &numero) { 
 
     for(int i=1 ; i<=10; i++){
-        numero =+ i;
+        numero = numero +i;
         cout << numero << "\n";
     }
 }
