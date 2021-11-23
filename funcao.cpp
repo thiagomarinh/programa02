c37157a07335a7b809bd4937d7bd544d92b21ccc
thiagomarinh <thiagoo_100@hotmail.com>
2021-11-23 18:22:10 -0300

    rand

--- funcao.cpp
+++ funcao.cpp
@@ -1,26 +1,28 @@
 #include <iostream> 
 #include <string>  
 #include <cstdlib>
+#include <ctime>
+using std::string;
+using std::cout;
 using namespace std;
 
-void incrementaNumero(int &);
-string forRange(string);
+void incrementaNumero(int &);    
+void usandoRange();
 
 int main(){
-    int numero = rand();
+
+    srand(time(NULL));              // gera uma semente de acordo com o tempo do computador
+    int numero = rand() % 100;    //gera numero aleatorio de 0 a 100
     incrementaNumero(numero);
-    
+    usandoRange();
     
 }
 
-string forRange(string){    
-    string str = "Mundo";
-
-    for(char c:str){
+void usandoRange(){ 
+    string str = "Mundo";          
+    for(char c:str)
         std::cout << c << endl;
-    }
 }
-
 
 void incrementaNumero(int &numero) { 
 
